edd3c19b6dcfca9d7e8b7fda0ec6a35177f0649f
Matteo Collina <matteo.collina@gmail.com>
2014-03-26 19:04:53 +0100

    Added working example.

---
+++ example.js
@@ -0,0 +1,20 @@
+
+var shard1  = { url: 'http://localhost:3000', append: true }
+  , shard2  = { url: 'http://localhost:3001', append: true }
+  , shard3  = { url: 'http://localhost:3002', append: false }
+  , sharder = require('./')({
+                shards: {
+                    1: shard1
+                  , 2: shard2
+                  , 3: shard3
+                }
+              })
+
+  , key     = sharder.generate()
+
+console.log(sharder.resolve(key))
+// { url: 'http://localhost:3000', append: true }
+console.log(sharder.resolve(key))
+// { url: 'http://localhost:3000', append: true }
+console.log(sharder.resolve(key))
+// { url: 'http://localhost:3000', append: true }
