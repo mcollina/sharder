3674ed1af80ebfe8430ab04d19a39fb54cff5899
Matteo Collina <matteo.collina@gmail.com>
2014-04-09 20:02:51 +0200

    Added cache mode.

--- example.js
+++ example.js
@@ -12,9 +12,26 @@ var shard1  = { url: 'http://localhost:3000', append: true }
 
   , key     = sharder.generate()
 
+  , cacher = require('./')({
+                cache: true,
+                shards: {
+                    1: shard1
+                  , 2: shard2
+                }
+              })
+
 console.log(sharder.resolve(key))
 // { url: 'http://localhost:3000', append: true, id: 1 }
 console.log(sharder.resolve(key))
 // { url: 'http://localhost:3000', append: true, id: 1 }
 console.log(sharder.resolve(key))
 // { url: 'http://localhost:3000', append: true, id: 1 }
+
+console.log(cacher.generate(new Buffer([1, 2])))
+// 01000000-0000-0000-0000-000000000102
+console.log(cacher.generate(new Buffer([1, 2])))
+// 01000000-0000-0000-0000-000000000102
+console.log(cacher.generate(new Buffer([3, 2])))
+// 02000000-0000-0000-0000-000000000302
+console.log(cacher.generate(new Buffer([3, 2])))
+// 02000000-0000-0000-0000-000000000302
--- test.js
+++ test.js
@@ -122,3 +122,49 @@ test('should not generate keys for the non-append shard', function(t) {
   t.equal(instance.resolve(instance.generate()), shard2)
   t.equal(instance.resolve(instance.generate()), shard1)
 })
+
+test('basic cache mode', function(t) {
+  t.plan(3)
+
+  var shard1    = { id: 1 }
+    , shard2    = { id: 2 }
+    , instance  = sharder({ cache: true, shards: { 1: shard1, 2: shard2 } })
+    , args      = new Buffer(5)
+    , key       = instance.generate(args)
+
+  t.equal(instance.generate(args), key)
+  t.type(key, 'string')
+  t.equal(key.length, 36)
+})
+
+test('throwing if in cache mode with a non-appendable shards', function(t) {
+  t.plan(1)
+
+  try {
+    sharder({ cache: true, shards: { 1: {} , 2: { append: false } } })
+  } catch(err) {
+    t.ok(err, 'must err')
+  }
+})
+
+test('cache encode to different shards', function(t) {
+
+  var shard1    = { id: 1 }
+    , shard2    = { id: 2 }
+    , instance  = sharder({ cache: true, shards: { 1: shard1, 2: shard2 } })
+
+  function doTest(prekey, expected) {
+    var key   = instance.generate(new Buffer(prekey))
+      , shard = instance.resolve(key)
+
+    console.log(key, shard)
+    t.equal(shard, expected)
+  }
+
+  doTest([1], shard2)
+  doTest([2], shard2)
+  doTest([3], shard1)
+  doTest([3, 1], shard2)
+
+  t.end()
+})
